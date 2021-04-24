703bfb71d6654a6071a84406b8ec92d4da5be2a1
Agustin-Venegas <68612141+Agustin-Venegas@users.noreply.github.com>
2021-04-24 11:12:33 -0400

    Añadir archivos
    Creado con QtCreator para Linux

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+int main()
+{
+
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
