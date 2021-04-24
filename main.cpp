0e9a11bee8ad8c945d294661a8d4cc68b7bb0931
Agustin-Venegas <avenegas19@alumnos.utalca.cl>
2021-04-24 14:23:15 -0400

    Abrir Ventana Ahora si deberia funcionar la compilación

--- main.cpp
+++ main.cpp
@@ -7,47 +7,44 @@ using namespace std; //the
 
 int main()
 {    
+	glewExperimental = true; // Needed for core profile
+	if( !glfwInit() )
+	{
+    	fprintf( stderr, "Failed to initialize GLFW\n" );
+    	return -1;
+	}
+
+	//variables de ventana
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //panes
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow( 1024, 768, "La Ventana", NULL, NULL);
+	if( window == NULL ){
+    	fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+    	glfwTerminate();
+    	return -1;
+	}
+
+	glfwMakeContextCurrent(window);
 
 
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-//variables de ventana
-glfwWindowHint(GLFW_SAMPLES, 4);
-glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //panes
-glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	do{ //ciclo principal de dibujado
+    	//Borrar la ventana con el color
+    	glClear( GL_COLOR_BUFFER_BIT );
 
-GLFWwindow* window;
-window = glfwCreateWindow( 1024, 768, "La Ventana", NULL, NULL);
-if( window == NULL ){
-    fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-    glfwTerminate();
-    return -1;
-}
+    	// Aqui va todo lo que se dibuja
 
-glfwMakeContextCurrent(window);
+    	// Cambiar buffers (imagen de antes/imagen de ahora)
+    	glfwSwapBuffers(window);
+    	glfwPollEvents();
 
-glewExperimental = true; // Needed for core profile
-if( !glfwInit() )
-{
-    fprintf( stderr, "Failed to initialize GLFW\n" );
-    return -1;
-}
-
-
-glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-do{
-    //Borrar la ventana con el color
-    glClear( GL_COLOR_BUFFER_BIT );
-
-    // Aqui va todo lo que se dibuja
-
-    // Cambiar buffers (imagen de antes/imagen de ahora)
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-
-} while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-       glfwWindowShouldClose(window) == 0 );
+	} while
+	(glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && !glfwWindowShouldClose(window));
 
     
     cout << "Hello World!" << endl;
