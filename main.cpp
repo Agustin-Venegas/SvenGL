aca938002184591d810d0c85dca382444afd0665
GabrielJimenezX3 <67122287+GabrielJimenezX3@users.noreply.github.com>
2021-04-24 12:01:12 -0400

    Prueba git
    puse la palabra panes

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main()
 //variables de ventana
 glfwWindowHint(GLFW_SAMPLES, 4);
 glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //pan
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //panes
 glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 GLFWwindow* window;
