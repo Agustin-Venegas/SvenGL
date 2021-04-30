8c17d5ca3afb55878761e8f36192dd1e5535b05f
GabrielJimenezX3 <67122287+GabrielJimenezX3@users.noreply.github.com>
2021-04-30 11:53:51 -0400

    HacerMatriz, Trasladar y Rotar
    wooow

--- Model.h
+++ Model.h
@@ -1,11 +1,11 @@
 #pragma once
 
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
 
 
 #include <stdlib.h>
@@ -20,4 +20,29 @@ class Model
 
     private:
     GLuint bufferObject;
+
+void HacerMatriz(){
+
+glm::mat4 myMatrix;
+glm::vec4 myVector;
+// fill myMatrix and myVector somehow
+glm::vec4 transformedVector = myMatrix * myVector; // Again, in this order ! this is important.
+
+}
+
+void Trasladar(){
+
+glm::mat4 myMatrix = glm::translate(glm::mat4(), glm::vec3(10.0f, 0.0f, 0.0f));
+glm::vec4 myVector(10.0f, 10.0f, 10.0f, 0.0f);
+glm::vec4 transformedVector = myMatrix * myVector; // guess the result
+
+}
+
+void Rotar(){
+
+glm::vec3 myRotationAxis( 5.f, 5.f, 5.f );
+glm::rotate( 5.f, myRotationAxis );
+
+}
+
 };
