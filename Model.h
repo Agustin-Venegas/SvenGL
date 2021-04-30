2f00cd98b0559af4469d989caa3d507eea1c2d6f
GabrielJimenezX3 <67122287+GabrielJimenezX3@users.noreply.github.com>
2021-04-30 13:06:30 -0400

    Funciones ahora en Model cpp
    oz

--- Model.h
+++ Model.h
@@ -15,6 +15,9 @@ class Model
     public:
     Model(); //crear modelo vacio
     Model(const float *vertex, bool color = false); //modelo sin colores
+    glm::mat4 matriz1;
+    
+
 
     GLuint getBO(); //regresa el bufferObject
 
@@ -26,25 +29,17 @@ class Model
 
 void HacerMatriz(){
 
-glm::mat4 myMatrix;
-glm::vec4 myVector;
-// fill myMatrix and myVector somehow
-glm::vec4 transformedVector = myMatrix * myVector; // Again, in this order ! this is important.
+
 
 }
 
-void Trasladar(){
-
-glm::mat4 myMatrix = glm::translate(glm::mat4(), glm::vec3(10.0f, 0.0f, 0.0f));
-glm::vec4 myVector(10.0f, 10.0f, 10.0f, 0.0f);
-glm::vec4 transformedVector = myMatrix * myVector; // guess the result
+glm::vec4 Trasladar(glm::vec4 myVector){
 
 }
 
-void Rotar(){
+void Rotar(glm::vec3 myRotationAxis, float){
 
-glm::vec3 myRotationAxis( 5.f, 5.f, 5.f );
-glm::rotate( 5.f, myRotationAxis );
+
 
 }
 
